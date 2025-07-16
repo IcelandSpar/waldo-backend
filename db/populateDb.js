cb3e24258f847b8463e218ade765675994c9dd9d
IcySpar <victorasielrodriguez@gmail.com>
2025-07-16 08:31:32 -0700

    feat: add schema to populate script

--- db/populateDb.js
+++ db/populateDb.js
@@ -3,10 +3,35 @@ require('dotenv').config();
 const { Client } = require('pg');
 
 
+
 const SQL = `
+DROP TABLE IF EXISTS leaderboard;
+DROP TABLE IF EXISTS images CASCADE;
+DROP TABLE IF EXISTS waldo_item;
+
 CREATE TABLE IF NOT EXISTS leaderboard (
-id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-name VARCHAR(12)
+player_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+name VARCHAR(12),
+start_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+end_time TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS images (
+image_id uuid DEFAULT gen_random_uuid(),
+image_name VARCHAR(255),
+image_path VARCHAR(255),
+difficulty VARCHAR(255),
+PRIMARY KEY(image_id)
+);
+
+CREATE TABLE IF NOT EXISTS waldo_item (
+waldo_item_id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+top_left_coord integer,
+bottom_right_coord integer,
+image_id uuid,
+CONSTRAINT fk_image
+  FOREIGN KEY(image_id)
+    REFERENCES images(image_id)
 );
 
 INSERT INTO leaderboard (name)
